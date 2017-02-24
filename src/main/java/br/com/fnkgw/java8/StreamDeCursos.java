b5b38e942a4a24e4711be7efa8f2874ecf8b46da
Fernando Nakagawa <fernandonkgw@gmail.com>
2017-02-24 08:15:49 -0300

    Stream, usando filter, map e foreach.

---
+++ src/main/java/br/com/fnkgw/java8/StreamDeCursos.java
@@ -0,0 +1,32 @@
+package br.com.fnkgw.java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class StreamDeCursos {
+
+	public static void main(String[] args) {
+		
+		List<Curso> cursos = new ArrayList<>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("Javascript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+		
+		cursos.stream()
+			.filter(c -> c.getAlunos() > 50)
+			.forEach(c -> System.out.println(c.getNome()));
+		
+		cursos.stream()
+			.map(Curso::getNome)
+			.forEach(System.out::println);
+		
+		cursos.stream()
+		   .filter(c -> c.getAlunos() > 50)
+		   .mapToInt(Curso::getAlunos)
+		   .forEach(System.out::println);
+	}
+}
